58d559d10d820077481b5c0417dacff88929b401
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2023-01-08 21:19:50 +0000

    update default v70 exps

--- base/update.web.now.sh
+++ base/update.web.now.sh
@@ -16,6 +16,8 @@ wcoss2.run.cmaq_pm25.sh \
 wcoss2.run.cmaq_o3.sh \
 wcoss2.run.rrfs_cmaq_o3.sh \
 wcoss2.run.rrfs_cmaq_pm25.sh \
+wcoss2.run.verif_cmaq_o3.sh \
+wcoss2.run.verif_cmaq_pm25.sh \
 )
 
 for i in "${site[@]}"; do
