95c71ea8b6eca10fff6acbee38797a59f7645e56
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2023-06-08 13:35:07 +0000

    incremental changes

--- base/update.web.now.sh
+++ base/update.web.now.sh
@@ -10,10 +10,10 @@ wcoss.run.cmaq_pm25_column.sh \
 wcoss.run.my_cmaq_pm25.sh \
 wcoss.run.my_cmaq_o3.sh \
 wcoss.run.global_aod.sh \
-)
-declare -a site=(
 wcoss2.run.cmaq_pm25.sh \
 wcoss2.run.cmaq_o3.sh \
+)
+declare -a site=(
 wcoss2.run.rrfs_cmaq_o3.sh \
 wcoss2.run.rrfs_cmaq_pm25.sh \
 wcoss2.run.verif_cmaq_o3.sh \
