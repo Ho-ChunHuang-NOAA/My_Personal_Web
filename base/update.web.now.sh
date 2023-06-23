144a3e36a2d593cb06a0726c7980165481770b5c
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2023-06-23 13:07:58 +0000

    trying to fix nug of 2019 and missing 2023 in *.base

--- base/update.web.now.sh
+++ base/update.web.now.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 declare -a bkp=(
-wcoss.run.hysplit_bluesky_p4s1.sh \
-wcoss.run.hysplit_bluesky_p4.sh \
-wcoss.run.hysplit_bluesky_xyz.sh \
-wcoss.run.hysplit_hms.sh \
-wcoss.run.hysplit_nco.sh \
-wcoss.run.hysplit_emc.sh \
-wcoss.run.cmaq_pm25_column.sh \
-wcoss.run.my_cmaq_pm25.sh \
-wcoss.run.my_cmaq_o3.sh \
 wcoss.run.global_aod.sh \
 )
 declare -a site=(
