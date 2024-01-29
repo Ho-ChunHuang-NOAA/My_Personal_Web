502c243f646709d05846fa1d18701b869a56c964
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-01-29 18:43:24 +0000

    add aqmv7 continue

--- base/update.web.date.sh
+++ base/update.web.date.sh
@@ -10,6 +10,7 @@ wcoss2.run.rrfs_cmaq_o3.sh \
 wcoss2.run.rrfs_cmaq_pm25.sh \
 wcoss2.run.eval_cmaq_o3.sh \
 wcoss2.run.eval_cmaq_pm25.sh \
+wcoss2_run_aqmv7_cmaq.sh \
 )
 
 for i in "${site[@]}"; do
