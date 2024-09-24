f92ff80564e27b19459109d07ffb734848acbb35
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-09-24 13:20:56 +0000

    incremental changes 09 24 2024

--- base/revise_web.sh
+++ base/revise_web.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "vi cmaq_pm25.base cmaq_pm25_max.base cmaq_pm25_sp.base cmaq_o3.base cmaq_o3_max.base"
+echo "vi rrfs_cmaq_pm25.base rrfs_cmaq_pm25_max.base rrfs_cmaq_pm25_sp.base rrfs_cmaq_o3.base rrfs_cmaq_o3_max.base"
