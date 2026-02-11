2d92e7273890558a5b77244763ee4bfa59c89864
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2026-02-11 15:36:19 +0000

    update for 2026 and clean up

--- base/wcoss2.run.rrfs_cmaq_o3.sh
+++ base/wcoss2.run.rrfs_cmaq_o3.sh
@@ -36,8 +36,6 @@ FIRST_AVAIL_DAY=20200901
 cdate=${TODAY}"00"
 FIRST_AVAIL_DAY=$(${NDATE} -2160 ${cdate} | cut -c1-8 )
 #
-FIRST_AVAIL_DAY=20240701
-#
 FstY0=`echo ${FIRST_AVAIL_DAY} | cut -c1-4`
 FstMx=$( echo ${FIRST_AVAIL_DAY} | cut -c5-6 | sed 's/^0//' )
 FstD0=`echo ${FIRST_AVAIL_DAY} | cut -c7-8`
